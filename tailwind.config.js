a32e98ed8083bd8c7df0a1dd4b01937e675483e9
aswath0001 <rsaswaths38@gmail.com>
2025-04-09 21:41:19 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: [
+    "./public/**/*.html",  // Scans HTML files
+    "./src/**/*.{js,jsx,ts,tsx}"  // Scans JS/JSX files
+  ],
+  theme:{
+    extend:{
+      fontFamily:{
+        zentry:['zentry','sans-serif'],
+        general:['general','sans-serif'],
+        'circular-web':['circular-web','sans-serif'],
+        'robert-medium':['robert-medium','sans-serif'],
+        'robert-regular':['robert-regular','sans-serif']
+      },
+      colors:{
+        blue:{
+          50:'#DFDFF0',
+          75:'#DFDFF2',
+          100:'#F0F2FA',
+          200:'#010101',
+          300:'#4FB7DD'
+        },
+        violet:{
+          300:'#5724FF',
+        },
+        yelow:{
+          100:'#8E983F',
+          300:'#EDFF66'
+        }
+      }
+    },
+  },
+}
